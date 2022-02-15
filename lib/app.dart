94200219e0e3d67f16f5ea23a61079abbf2bbc0a
Nishat Nasir <nishatnasir00@gmail.com>
2022-02-15 16:24:40 +0900

    Added

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'presentation/template/template.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'My Page'),
+    );
+  }
+}
---
+++ lib/presentation/template/template.dart
@@ -0,0 +1,5 @@
+export 'package:flutter/material.dart';
+
+//pages
+export '../../app.dart';
+export '../pages/homePage.dart';
